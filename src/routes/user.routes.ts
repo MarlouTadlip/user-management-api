69ae29ed26fa860080b730e7862ad3efcfb3664c
unknown <YY338@ccs.local>
2025-03-20 05:59:41 -0700

    change

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -2,15 +2,4 @@ import { Router } from "express"
 
 export const userRouter = Router()
 
-//GET ALL USERS
-userRouter.get('/')
-
-//GET ONE USER
-userRouter.get('/:id')
-
-//CREATE ONE USER
-userRouter.post('/')
-
-//DELETE ONE USER
-userRouter.delete('/:id')
 
