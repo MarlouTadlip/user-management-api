4cce9807ae0c631433b839cf568c2534be76ca69
Marlo Gallego <Marlochinito2004@gmail.com>
2025-03-25 12:33:53 +0800

    Modifying deleteUser controller

--- src/controllers/user.controllers.ts
+++ src/controllers/user.controllers.ts
@@ -6,5 +6,6 @@ const userRepository = AppDataSource.getRepository(User);
 export const deleteUser = (req: Request, res: Response) => { 
     
     userRepository.delete(req.params.id)
+    res.json({ message: "User deleted" });
 }
 
