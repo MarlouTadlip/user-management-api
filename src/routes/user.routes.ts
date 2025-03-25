ab00b0a8b9c485e96f6ef454ad1d34eff343efa7
MarlouTadlip <tadlipmarlou@gmail.com>
2025-03-25 10:35:33 +0800

    changes

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,16 +1,3 @@
-import { Router } from "express"
+import { Router } from "express";
 
-export const userRouter = Router()
-
-//GET ALL USERS
-userRouter.get('/')
-
-//GET ONE USER
-userRouter.get('/:id')
-
-//CREATE ONE USER
-userRouter.post('/')
-
-//DELETE ONE USER
-userRouter.delete('/:id')
-
+export const userRouter = Router();
