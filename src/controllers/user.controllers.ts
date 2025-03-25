def96d38be61cb98752a59e335bae9f409cd702c
unknown <HH402@ccs.local>
2025-03-25 18:11:34 +0800

    Added GetAllUser and GetUserById

--- src/controllers/user.controllers.ts
+++ src/controllers/user.controllers.ts
@@ -8,3 +8,15 @@ export const deleteUser = (req: Request, res: Response) => {
     userRepository.delete(req.params.id)
     res.json({ message: "User deleted" });
 }
+
+// GET ALL USERS
+export const getAllUsers = async (req: Request, res: Response) => {
+    const users = await userRepository.find();
+    res.json({ message: "User List", users });
+};
+
+// GET USER BY ID
+export const getUserById = async (req: Request, res: Response) => {
+    const user = await userRepository.findOneBy({ id: parseInt(req.params.id) });
+    res.json({ message: "User Found", user });
+};
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,8 +1,14 @@
 import { Router } from "express"
 import  {deleteUser}  from "../controllers/user.controllers"
+import { getAllUsers, getUserById } from "../controllers/user.controllers";
 
 export const userRouter = Router()
 
 //DELETE ONE USER
 userRouter.delete('/:id', deleteUser)
 
+// GET ALL USERS
+userRouter.get("/", getAllUsers);
+
+// GET ONE USER BY ID
+userRouter.get("/:id", getUserById);
