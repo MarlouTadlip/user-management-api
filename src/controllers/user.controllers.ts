fd31f6603729176a2ca6ef599cf74379a3cdef62
Marlo Gallego <Marlochinito2004@gmail.com>
2025-03-25 11:24:41 +0800

    Added delete controler and route

--- src/controllers/user.controllers.ts
+++ src/controllers/user.controllers.ts
@@ -2,3 +2,9 @@ import { AppDataSource } from "../data-source";
 import { User } from "../entities/User";
 
 const userRepository = AppDataSource.getRepository(User);
+const deleteUser = (req: Request, res: Response) => { 
+    
+    userRepository.delete(req.params.id)
+}
+
+export default deleteUser;
